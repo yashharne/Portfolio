4790aaf077eea83aa92f84fa978ac20d8a456130
yashharne <yashharne.ed@gmail.com>
2024-01-21 02:17:58 +0530

    style: improve skills section responsiveness

--- src/components/main/Navbar.tsx
+++ src/components/main/Navbar.tsx
@@ -4,9 +4,9 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div className='w-full h-[65px] fixed top-0 shawdow-lg shawdow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10'>
-            <div className='w-full h-full flex flex-row items-center justify-between m-auto sm:px-[10px]'>
-                <a href='#about-me' className='h-auto w-auto flex flex-row items-center'>
+        <div className='w-full h-[65px] fixed top-0 shawdow-lg shawdow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-6 sm:px-10'>
+            <div className='w-full h-full flex flex-row items-center justify-between gap-4 sm:gap-0 m-auto sm:px-[10px]'>
+                <a href='#about-me' className='h-auto w-auto flex flex-row items-center z-100'>
                     <Image
                         src="/logo1.png"
                         alt='logo'
--- src/components/main/Skills.tsx
+++ src/components/main/Skills.tsx
@@ -6,14 +6,14 @@ import { BackendSkills, FrontendSkills, OtherSkills } from '@/constants'
 import SkillText from '../sub/SkillText'
 
 const Skills = () => {
-    var width = 1000;
+    // var width = 1000;
 
-    if (typeof globalThis?.window?.innerWidth !== 'undefined') {
-        width = globalThis.window.innerWidth;
-    }
+    // if (typeof globalThis?.window?.innerWidth !== 'undefined') {
+    //     width = globalThis.window.innerWidth;
+    // }
 
-    const isMobileDevice = width <= 980;
-    const isMobilePhone = width <= 425;
+    // const isMobileDevice = width <= 980;
+    // const isMobilePhone = width <= 425;
 
     return (
         <section
@@ -21,66 +21,198 @@ const Skills = () => {
             className='flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden py-10 sm:py-40 transform scale-90 z-[20]'
         >
             <SkillText />
-            <div className='flex flex-row justify-around flex-wrap mt-10 sm:mt-4 gap-5 items-center z-[30]'>
-                {
-                    FrontendSkills.map((skill, index) => (
-                        <SkillsDataProvider
-                            key={index}
-                            src={skill.src}
-                            name={skill.name}
-                            index={index}
-                            width={isMobileDevice ? (isMobilePhone ? skill.width * 0.6 : skill.width * 0.8) : skill.width}
-                            height={isMobileDevice ? (isMobilePhone ? skill.height * 0.6 : skill.height * 0.8) : skill.height}
-                            scale={skill.scale || 1}
+            <div className='large-screens hidden md:flex flex-col items-center justify-center'>
+                <div className='flex flex-row justify-around flex-wrap mt-10 sm:mt-4 gap-5 items-center z-[30]'>
+                    {
+                        FrontendSkills.map((skill, index) => (
+                            <SkillsDataProvider
+                                key={index}
+                                src={skill.src}
+                                name={skill.name}
+                                index={index}
+                                width={skill.width}
+                                height={skill.height}
+                                scale={skill.scale || 1}
+                            />
+                        ))
+                    }
+                </div>
+
+                <div className='flex flex-row justify-around flex-wrap mt-10 sm:mt-4 gap-5 items-center z-[30]'>
+                    {
+                        OtherSkills.map((skill, index) => (
+                            <SkillsDataProvider
+                                key={index}
+                                src={skill.src}
+                                name={skill.name}
+                                index={index}
+                                width={skill.width}
+                                height={skill.height}
+                                scale={skill.scale || 1}
+                            />
+                        ))
+                    }
+                </div>
+
+                <div className='flex flex-row justify-around flex-wrap mt-10 sm:mt-4 gap-5 items-center z-[30]'>
+                    {
+                        BackendSkills.map((skill, index) => (
+                            <SkillsDataProvider
+                                key={index}
+                                src={skill.src}
+                                name={skill.name}
+                                index={index}
+                                width={skill.width}
+                                height={skill.height}
+                                scale={skill.scale || 1}
+                            />
+                        ))
+                    }
+                </div>
+
+                <div className='w-full h-full absolute'>
+                    <div className='w-full h-full z-[15] opacity-30 absolute flex items-center justify-center bg-cover'>
+                        <video
+                            className='w-full h-auto'
+                            preload='false'
+                            playsInline
+                            loop
+                            muted
+                            autoPlay
+                            src='/cards-video.webm'
+
                         />
-                    ))
-                }
+                    </div>
+                </div>
             </div>
 
-            <div className='flex flex-row justify-around flex-wrap mt-10 sm:mt-4 gap-5 items-center z-[30]'>
-                {
-                    OtherSkills.map((skill, index) => (
-                        <SkillsDataProvider
-                            key={index}
-                            src={skill.src}
-                            name={skill.name}
-                            index={index}
-                            width={isMobileDevice ? (isMobilePhone ? skill.width * 0.6 : skill.width * 0.8) : skill.width}
-                            height={isMobileDevice ? (isMobilePhone ? skill.height * 0.6 : skill.height * 0.8) : skill.height}
-                            scale={skill.scale || 1}
+            <div className='tablets hidden sm:block md:hidden'>
+                <div className='flex flex-row justify-around flex-wrap mt-10 sm:mt-4 gap-5 items-center z-[30]'>
+                    {
+                        FrontendSkills.map((skill, index) => (
+                            <SkillsDataProvider
+                                key={index}
+                                src={skill.src}
+                                name={skill.name}
+                                index={index}
+                                width={skill.width * 0.8}
+                                height={skill.height * 0.8}
+                                scale={skill.scale || 1}
+                            />
+                        ))
+                    }
+                </div>
+
+                <div className='flex flex-row justify-around flex-wrap mt-10 sm:mt-4 gap-5 items-center z-[30]'>
+                    {
+                        OtherSkills.map((skill, index) => (
+                            <SkillsDataProvider
+                                key={index}
+                                src={skill.src}
+                                name={skill.name}
+                                index={index}
+                                width={skill.width * 0.8}
+                                height={skill.height * 0.8}
+                                scale={skill.scale || 1}
+                            />
+                        ))
+                    }
+                </div>
+
+                <div className='flex flex-row justify-around flex-wrap mt-10 sm:mt-4 gap-5 items-center z-[30]'>
+                    {
+                        BackendSkills.map((skill, index) => (
+                            <SkillsDataProvider
+                                key={index}
+                                src={skill.src}
+                                name={skill.name}
+                                index={index}
+                                width={skill.width * 0.8}
+                                height={skill.height * 0.8}
+                                scale={skill.scale || 1}
+                            />
+                        ))
+                    }
+                </div>
+
+                <div className='w-full h-full absolute'>
+                    <div className='w-full h-full z-[15] opacity-30 absolute flex items-center justify-center bg-cover'>
+                        <video
+                            className='w-full h-auto'
+                            preload='false'
+                            playsInline
+                            loop
+                            muted
+                            autoPlay
+                            src='/cards-video.webm'
+
                         />
-                    ))
-                }
+                    </div>
+                </div>
             </div>
 
-            <div className='flex flex-row justify-around flex-wrap mt-10 sm:mt-4 gap-5 items-center z-[30]'>
-                {
-                    BackendSkills.map((skill, index) => (
-                        <SkillsDataProvider
-                            key={index}
-                            src={skill.src}
-                            name={skill.name}
-                            index={index}
-                            width={isMobileDevice ? (isMobilePhone ? skill.width * 0.6 : skill.width * 0.8) : skill.width}
-                            height={isMobileDevice ? (isMobilePhone ? skill.height * 0.6 : skill.height * 0.8) : skill.height}
-                            scale={skill.scale || 1}
+            <div className='mobile-screens sm:hidden'>
+                <div className='flex flex-row justify-around flex-wrap mt-10 sm:mt-4 gap-5 items-center z-[30]'>
+                    {
+                        FrontendSkills.map((skill, index) => (
+                            <SkillsDataProvider
+                                key={index}
+                                src={skill.src}
+                                name={skill.name}
+                                index={index}
+                                width={skill.width * 0.6}
+                                height={skill.height * 0.6}
+                                scale={skill.scale || 1}
+                            />
+                        ))
+                    }
+                </div>
+
+                <div className='flex flex-row justify-around flex-wrap mt-10 sm:mt-4 gap-5 items-center z-[30]'>
+                    {
+                        OtherSkills.map((skill, index) => (
+                            <SkillsDataProvider
+                                key={index}
+                                src={skill.src}
+                                name={skill.name}
+                                index={index}
+                                width={skill.width * 0.6}
+                                height={skill.height * 0.6}
+                                scale={skill.scale || 1}
+                            />
+                        ))
+                    }
+                </div>
+
+                <div className='flex flex-row justify-around flex-wrap mt-10 sm:mt-4 gap-5 items-center z-[30]'>
+                    {
+                        BackendSkills.map((skill, index) => (
+                            <SkillsDataProvider
+                                key={index}
+                                src={skill.src}
+                                name={skill.name}
+                                index={index}
+                                width={skill.width * 0.6}
+                                height={skill.height * 0.6}
+                                scale={skill.scale || 1}
+                            />
+                        ))
+                    }
+                </div>
+
+                <div className='w-full h-full absolute'>
+                    <div className='w-full h-full z-[15] opacity-30 absolute flex items-center justify-center bg-cover'>
+                        <video
+                            className='w-full h-auto'
+                            preload='false'
+                            playsInline
+                            loop
+                            muted
+                            autoPlay
+                            src='/cards-video.webm'
+
                         />
-                    ))
-                }
-            </div>
-
-            <div className='w-full h-full absolute'>
-                <div className='w-full h-full z-[15] opacity-30 absolute flex items-center justify-center bg-cover'>
-                    <video
-                        className='w-full h-auto'
-                        preload='false'
-                        playsInline
-                        loop
-                        muted
-                        autoPlay
-                        src='/cards-video.webm'
-
-                    />
+                    </div>
                 </div>
             </div>
         </section >
